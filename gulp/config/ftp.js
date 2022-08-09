1d839db93883622b031eac37e837167b22f93b26
ALEXANDER DUBOVTSEV <alexdubovtsev.it@gmail.com>
2022-08-09 12:59:06 +0200

    Finished Gulp build

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "", // FTP server address
+    user: "", // User name
+    password: "", // Password
+    parallel: 15, // Number of parallel threads
+}
--- gulpfile.js
+++ gulpfile.js
@@ -26,11 +26,12 @@ import { images } from './gulp/tasks/images.js';
 import { otfToTtf, ttfToWoff, fontStyle } from './gulp/tasks/fonts.js';
 import { svgSprive } from './gulp/tasks/svgSprive.js';
 import { zip } from './gulp/tasks/zip.js';
+import { ftp } from './gulp/tasks/ftp.js';
 
 // Changes watcher
 function watcher() {
     gulp.watch(path.watch.files, copy);
-    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch.images, images);
@@ -48,11 +49,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 // Script export
 export { dev }
 export { build }
 export { deployZIP }
+export { deployFTP }
 
 // Default script execution
 gulp.task('default', dev);
