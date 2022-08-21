316bea40163220e95bd4532a86c2f5afe0fe7f68
ALEXANDER DUBOVTSEV <alexdubovtsev.it@gmail.com>
2022-08-21 21:48:17 +0200

    Add JS libs

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,162 @@
-import * as Functions from "./modules/functions.js"; 
-Functions.isWebp();
+// FLS (Full Logging System) 
+window['FLS'] = true;
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import "../scss/style.scss";
 
-const swiper = new Swiper();
+// todo Main Modules
+
+import * as functions from "./modules/functions.js";
+
+/* Check webp support, add webp or no-webp class for HTML */
+functions.isWebp();
+
+/* Add touch class for HTML if the browser is mobile */
+// functions.addTouchClass();
+
+/* Add loaded class for HTML after full page load */
+// functions.addLoadedClass();
+
+/* Floating bar on mobile devices at 100vh */
+// functions.fullVHfix();
+
+/* Module Burger-menu */
+// functions.burgerMenu();
+
+/* Module Spollers */
+// functions.spollers();
+
+/* Module Show More */
+// functions.showMore();
+
+/* Module Tabs */
+// functions.tabs();
+
+/* Module Popups */
+// import './libs/popup.js'
+
+/* Module Mouse parallax */
+// import './libs/parallax-mouse.js'
+
+// todo Forms
+
+import * as forms from "./modules/forms/forms.js";
+
+forms.formFieldsInit({
+	viewPass: false,
+	autoHeight: false
+});
+
+
+/* Oтправка формы */
+/* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
+// flsForms.formSubmit();
+
+/* Модуль формы "колличество" */
+// flsForms.formQuantity();
+
+/* Модуль звездного рейтинга */
+// flsForms.formRating();
+
+/* Модуль работы с select. */
+// import './libs/select.js'
+
+/* Модуль работы с календарем */
+// import './files/forms/datepicker.js'
+
+/* (В работе) Модуль работы с масками.*/
+/*
+Подключение и настройка выполняется в файле js/files/forms/inputmask.js
+Документация по работе в шаблоне:
+Документация плагина: https://github.com/RobinHerbots/inputmask
+Сниппет(HTML):
+*/
+// import "./files/forms/inputmask.js";
+
+/* Модуль работы с ползунком */
+/*
+Подключение и настройка выполняется в файле js/files/forms/range.js
+Документация по работе в шаблоне:
+Документация плагина: https://refreshless.com/nouislider/
+Сниппет (HTML): range
+*/
+// import "./files/forms/range.js";
+
+/* Модуль работы с подсказками (tippy) */
+/*
+Подключение плагина Tippy.js и настройка выполняется в файле js/files/tippy.js
+Документация по работе в шаблоне:
+Документация плагина: https://atomiks.github.io/tippyjs/
+Сниппет (HTML): tip (добавляет атрибут с подсказкой для html тега)
+*/
+// import "./files/tippy.js";
+
+
+// todo Slider (Swiper)
+
+ /*
+Настройка подключения плагина слайдера Swiper и новых слайдеров выполняется в файле js/files/sliders.js
+Документация по работе в шаблоне: https://template.fls.guru/template-docs/rabota-so-slajderom-swiper.html
+Документация плагина: https://swiperjs.com/
+Сниппет(HTML): swiper
+*/
+// import "./modules/sliders.js";
+
+
+// todo Scroll
+
+/*
+Изменение дизайна скроллбара
+Документация по работе в шаблоне: В HTML добавляем к блоку атрибут data-simplebar
+Документация плагина: https://github.com/Grsmto/simplebar/tree/master/packages/simplebar
+Сниппет(HTML): 
+*/
+// import './files/scroll/simplebar.js';
+
+// Ленивая (отложенная) загрузка картинок
+// Документация по работе в шаблоне: https://template.fls.guru/template-docs/modul-lenivaya-podgruzka-lazy-loading.html
+// Документация плагина: https://github.com/verlok/vanilla-lazyload
+// Сниппет(HTML):
+// import './files/scroll/lazyload.js';
+
+// Наблюдатель за объектами c атрибутом data-watch
+// Документация: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html
+// Сниппет(HTML):
+// import './libs/watcher.js'
+
+// Функции работы скроллом
+import * as flsScroll from "./files/scroll/scroll.js";
+
+// Плавная навигация по странице
+// Документация: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
+// flsScroll.pageNavigation();
+
+// Функционал добавления классов к хедеру при прокрутке
+// Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
+// flsScroll.headerScroll();
+
+// Функционал липкого блока
+// flsScroll.stickyBlock();
+
+
+// todo Gallery (lightgalleryjs.com)
+
+// import "./modules/gallery.js"; 
+/*
+Документация по работе в шаблоне: 
+Документация плагина: https://www.lightgalleryjs.com/docs/
+Сниппет(HTML):
+*/
+// import "./files/gallery.js";
+
+// todo My code
+
+/* Динамический адаптив */
+// Документация: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
+// import "./libs/dynamic_adapt.js";
+
+/* Форматирование чисел */
+// import './libs/wNumb.min.js';
+
+import "./modules/script.js"; 
+
 
